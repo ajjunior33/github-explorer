af5bb6351c570fa06a00988b281e36241597c18c
Andre Souza <ajjunior33@gmail.com>
2021-01-10 18:02:35 -0300

    Navegando entre rotas.

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, FormEvent } from "react";
+import { Link } from 'react-router-dom'
 import { FiChevronRight } from "react-icons/fi";
 
 import logoImg from "../../assets/logo.svg";
@@ -29,7 +30,7 @@ const Dashboard: React.FC = () => {
   });
   const [newRepo, setNewRepo] = useState("");
   const [inputError, setInputError] = useState("");
-  
+
   useEffect(() => {
     localStorage.setItem(
       "@GithubExplorer:repositories",
@@ -73,7 +74,7 @@ const Dashboard: React.FC = () => {
       {inputError && <Error>{inputError} </Error>}
       <Repositories>
         {repositories.map((repository) => (
-          <a href="teste" key={repository.full_name}>
+          <Link  to={`/repository/${repository.full_name}`} key={repository.full_name}>
             <img
               src={repository.owner.avatar_url}
               alt={repository.owner.login}
@@ -83,7 +84,7 @@ const Dashboard: React.FC = () => {
               <p>{repository.description}</p>
             </div>
             <FiChevronRight size={20} />
-          </a>
+          </Link>
         ))}
       </Repositories>
     </>
